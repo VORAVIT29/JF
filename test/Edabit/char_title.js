af1f512b7c474039841a23db4ae205892bb67c9a
VORAVIT29 <6206021610153@fitm.kmutnb.ac.th>
2021-02-12 16:36:07 +0700

    Edit 12/2/64

--- test/Edabit/char_title.js
+++ test/Edabit/char_title.js
@@ -1,19 +1,53 @@
+//Open function Check letters special
+function check_letters_special(arry, space) {
+    let sum = ""
+
+    for (let out of arry) {
+
+        if (out == "in" || out == "and" || out == "the" || out == "of") {
+            sum += space + out
+            continue;
+        }
+
+        sum += space + out[0].toUpperCase() + out.slice(1)
+    }
+
+    return (sum)
+} //close function check letters special
+
+
+
+
+//open function char frist
 function char_frist(str) {
+    let check = str.match(/[-]/gi)
     let text = ""
 
     str = str.toLowerCase().split(' ')
 
-    for (let out of str) {
-        if (out == "in" || out == "and" || out == "the" || out == "of") {
-            text += " " + out
-            continue;
-        }
-        text += " " + out[0].toUpperCase() + out.slice(1)
+    //Get function check letters special
+    text = check_letters_special(str, ' ')
+
+    //Check Not Letters Spacial
+    if (check === undefined || check === null) {
+        return (text.slice(1))
     }
-    return (text.slice(1))
-}
+
+    let text2 = text.split(',').slice(1).toString().split('-')
+    let totaltext = ""
+
+    totaltext = check_letters_special(text2, "-")
+
+    totaltext = totaltext.slice(1)
+    text = text.slice(1).split(',')
+    text[1] = totaltext
+
+    return (text.join(','))
+
+} //close function char frist
 
 
 //RUN
 let str = "MANCE RAYDER, KING-BEYOND-THE-WALL."
+let str1 = "Doran Martell, Prince of Dorne, Lord of Sunspear."
 console.log(char_frist(str))
